5efb5fd61b59b25591eef6771726d838508977a5
chakane <cs499@njit.edu>
2021-11-27 23:46:04 -0500

    refactor URLSession wrapper and ProjectAPI

--- POSTY/Network Services/NetworkHelper.swift
+++ POSTY/Network Services/NetworkHelper.swift
@@ -7,20 +7,29 @@
 
 import Foundation
 
+// utilize enum to switch state of network call
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
+
 class NetworkHelper {
     static let manager = NetworkHelper()
     
-    func getData(from url: URL, completionHandler: @escaping ((Result<Data, AppError>) -> Void)) {
-        self.urlSession.dataTask(with: url) { (data, response, error) in
-            
+    func performDataTask(withUrl url: URL, andHTTPBody body: Data? = nil, andMethod httpMethod: HTTPMethod, completionHandler: @escaping ((Result<Data, AppError>) -> Void)) {
+        var request = URLRequest(url: url)
+        request.httpMethod = httpMethod.rawValue
+        request.httpBody = body
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        urlSession.dataTask(with: request) { (data, response, error) in
             DispatchQueue.main.async {
                 guard let data = data else {
                     completionHandler(.failure(.noDataReceived))
                     return
                 }
                 
-                
-                guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                guard let response = response as? HTTPURLResponse, (200...299) ~= response.statusCode else {
                     completionHandler(.failure(.badStatusCode))
                     return
                 }
@@ -40,6 +49,6 @@ class NetworkHelper {
         }.resume()
     }
     
-    private init() {}
     private let urlSession = URLSession(configuration: URLSessionConfiguration.default)
+    private init() {}
 }
--- POSTY/Network Services/ProjectAPIClient.swift
+++ POSTY/Network Services/ProjectAPIClient.swift
@@ -10,7 +10,7 @@ import Foundation
 struct ProjectAPIClient {
     static let manager = ProjectAPIClient()
     func getProjects(completionHandler: @escaping (Result<[Project], AppError>) -> Void) {
-        NetworkHelper.manager.getData(from: airtableURL) { result in
+        NetworkHelper.manager.performDataTask(withUrl: airtableURL, andMethod: .get) { result in
             switch result {
             case let .failure(error):
                 completionHandler(.failure(error))
@@ -26,12 +26,29 @@ struct ProjectAPIClient {
         }
     }
     
+    func post(_ project: Project, completionHandler: @escaping (Result<Data, AppError>) -> Void) {
+        let projectWrapper = ProjectWrapper(project: project)
+        guard let encodedProjectWrapper = try? JSONEncoder().encode(projectWrapper) else {
+            fatalError("Unable to encode json project")
+        }
+        print(String(data: encodedProjectWrapper, encoding: .utf8)!)
+        NetworkHelper.manager.performDataTask(withUrl: airtableURL, andHTTPBody: encodedProjectWrapper, andMethod: .post, completionHandler: { result in
+            switch result {
+            case let .success(data):
+                completionHandler(.success(data))
+            case let .failure(error):
+                completionHandler(.failure(error))
+            }
+        })
+    }
+    
     private var airtableURL: URL {
         guard let url = URL(string: "https://api.airtable.com/v0/appPySJxlJPSYRDit/Design%20projects?api_key=" + SecretKey.APIKey) else {
             fatalError("Error: invald url")
         }
         return url
     }
+    private init() {}
 }
 
 
